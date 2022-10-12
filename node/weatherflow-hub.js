6d75ec1c5241a64467c45908e430728144bcd016
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-10-12 20:17:47 +0200

    add status events

--- node/weatherflow-hub.js
+++ node/weatherflow-hub.js
@@ -1,8 +1,43 @@
 module.exports = function (RED) {
+    var dgram = require('dgram');
+
+    var udpClient = null;
+
     function WeatherFlowHubNode(config) {
         RED.nodes.createNode(this, config);
         this.port = config.port;
+        var node = this;
+
+        udpClient = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+
+        udpClient.on('close', function () {
+            node.emit("status", { fill: "grey", shape: "dot", text: "close" });
+        });
+
+        udpClient.on('connect', function () {
+            node.emit("status", { fill: "yellow", shape: "dot", text: "connect" });
+        });
+
+        udpClient.on('error', function (error) {
+            node.error(error);
+            node.emit("status", { fill: "red", shape: "ring", text: "error" });
+        });
+
+        udpClient.on('listening', function () {
+            node.emit("status", { fill: "green", shape: "dot", text: "listening" });
+        });
+
+        udpClient.on('message', function (message, remote) {
+            //node.log(remote.address + ':' + remote.port + ' - ' + message);
+        });
+
+        udpClient.bind({
+            address: '0.0.0.0',
+            port: config.port,
+            exclusive: false
+        });
     }
-    
+
+
     RED.nodes.registerType("weatherflow-hub", WeatherFlowHubNode);
 }
--- node/weatherflow-udp.js
+++ node/weatherflow-udp.js
@@ -2,6 +2,13 @@ module.exports = function (RED) {
     function UdpListenerNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+
+        // Retrieve the hub node
+        node.hub = RED.nodes.getNode(config.hub);
+
+        node.hub.on("status", function (status) {
+            node.status(status);
+        });
     }
 
     RED.nodes.registerType("weatherflow-udp", UdpListenerNode);
